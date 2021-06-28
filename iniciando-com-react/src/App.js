39158ec1ca03af4c4b66e051f3d5b58bb0e90a10
samueljbp <samuel_jbp@yahoo.com.br>
2021-06-28 09:58:45 -0300

    Criação do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: ''
+  }
+
+  modificarNome = (event) => {
+    let nome = event.target.value;
+    this.setState({
+      nome: nome
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = ["Fulano", "Cicrano", "Beltrano"]
+    const comboBoxOptions = opcoes.map(opcao => <option>{opcao}</option>)
+
+    return (
+      <select>
+        {comboBoxOptions}
+      </select>
+    )
+  }
+
+  componentDidMount() {
+    console.log("Executou o componentDidMount");
+  }
+
+  render() {
+    const MeuComboBox = () => this.criaComboBox();
+
+    return (
+      <>
+        <input type="text" className="texto-centralizado" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello {this.state.nome} e tome uma prop {this.props.nome} idade {this.props.idade}</h1>
+        <MeuComboBox></MeuComboBox>
+      </>
+
+    )
+  }
 }
 
 export default App;
